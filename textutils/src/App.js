17377955375b9f7890ec60cbd93fe00e2f77d53b
Fiza Hussain <hussainfiza57@gmail.com>
2023-09-04 20:01:26 +0500

    video6

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -4,7 +4,8 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-      <Navbar title= "BestTextUtils"/>
+      <Navbar title= "BestTextUtils" aboutText="About Us"/>
+      
     </>
   );
 }
