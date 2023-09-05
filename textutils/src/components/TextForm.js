f2310af21f8f98c06cb78f6241d69ff3ed236011
Fiza Hussain <hussainfiza57@gmail.com>
2023-09-05 09:40:38 +0500

    added custom buttons

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -2,14 +2,50 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        console.log("Uppercase Btn was clicked");
+        // console.log("Uppercase Btn was clicked");
         let newText = text.toUpperCase();
         setText(newText);
     }
 
     const handleLowClick = ()=>{
-        console.log("Lowercase Btn was clicked");
+        // console.log("Lowercase Btn was clicked");
         let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleClearText = ()=>{
+        // console.log("Lowercase Btn was clicked");
+        let newText = '';
+        setText(newText);
+    }
+
+    const handleCapitalCase = ()=>{
+        // console.log("Lowercase Btn was clicked");
+        let newText = '';
+        let textArr = text.split(" ");
+        for (let index = 0; index < textArr.length; index++) {
+            textArr[index] =  textArr[index].charAt(0).toUpperCase() + textArr[index].slice(1);
+            newText += textArr[index] + " ";
+        }
+        setText(newText);
+    }
+
+    const handleAltCase = ()=>{
+        // debugger
+        let newText = '';
+        let textArr = text.split(" ");
+        for (let index = 0; index < textArr.length; index++) {
+            let newWord = '';
+            for (let i = 0; i < textArr[index].length; i++) {
+                if (i%2===0) {
+                    newWord += textArr[index].charAt(i).toLowerCase();
+                }
+                else {
+                    newWord += textArr[index].charAt(i).toUpperCase();
+                }
+            }
+            newText += newWord + " ";
+        }
         setText(newText);
     }
 
@@ -25,8 +61,12 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLowClick}>Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleCapitalCase}>Capitalized Case</button>
+            <button className="btn btn-primary mx-2" onClick={handleAltCase}>aLtErNaTiNg cAsE</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
+
         </div>
         <div className="container my-3">
             <h2>Your Text Summary:</h2>
